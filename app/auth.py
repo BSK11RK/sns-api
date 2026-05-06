853d326ab457a0e2dd630d7b5305417b015e30f1
rk.dev <rekubota.dev@gmail.com>
2026-05-06 10:56:47 +0900

    Docker環境を構築し、bcryptのバージョンを修正してパスワードエラーに対応

--- app/auth.py
+++ app/auth.py
@@ -1,11 +1,15 @@
+import os
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from jose import JWTError, jwt
+from dotenv import load_dotenv
 
 
-SECRET_KEY = "secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context =CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -31,7 +35,6 @@ def create_access_token(data: dict):
 # JWT検証
 def decode_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except JWTError:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except:
         return None
